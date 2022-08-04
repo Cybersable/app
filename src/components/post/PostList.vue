95d064e03516781be985b0f346d429462f8d8470
Evgeniy <xmaoural@gmail.com>
2022-08-04 15:18:10 +0500

    Add post list title changing if post list is empty

--- src/components/post/PostList.vue
+++ src/components/post/PostList.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="my-4">
-    <h3>Posts list</h3>
-    <div class="row row-cols-1 row-cols-md-2 g-4 pt-4 pb-4">
-      <div class="col" v-for="post in posts" :key="post.id">
-        <PostItem :post="post" @deletePost="$emit('deletePost', post)" />
+    <template v-if="posts.length > 0">
+      <h3>Posts list</h3>
+      <div class="row row-cols-1 row-cols-md-2 g-4 pt-4 pb-4">
+        <div class="col" v-for="post in posts" :key="post.id">
+          <PostItem :post="post" @deletePost="$emit('deletePost', post)" />
+        </div>
       </div>
-    </div>
+    </template>
+    <template v-else>
+      <h3>Post list is empty</h3>
+    </template>
   </div>
 </template>
 
