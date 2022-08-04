368f34b8d2e3a28a9350d6a9c1bd569e48d643eb
Evgeniy <xmaoural@gmail.com>
2022-08-04 19:25:02 +0500

    Add transition to post list

--- src/components/post/PostList.vue
+++ src/components/post/PostList.vue
@@ -3,9 +3,11 @@
     <template v-if="posts.length > 0">
       <h3>Posts list</h3>
       <div class="row row-cols-1 row-cols-md-2 g-4 pt-4 pb-4">
-        <div class="col" v-for="post in posts" :key="post.id">
-          <PostItem :post="post" @deletePost="$emit('deletePost', post)" />
-        </div>
+        <transition-group name="post-list">
+          <div class="col" v-for="post in posts" :key="post.id">
+            <PostItem :post="post" @deletePost="$emit('deletePost', post)" />
+          </div>
+        </transition-group>
       </div>
     </template>
     <template v-else>
@@ -27,3 +29,15 @@ export default {
   emits: ['deletePost']
 }
 </script>
+
+<style>
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.3s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+</style>
