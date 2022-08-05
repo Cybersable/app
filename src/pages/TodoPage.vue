87245c8b986f27c32f531734b6337cef6c8cbd07
Evgeniy <xmaoural@gmail.com>
2022-08-05 10:37:11 +0500

    Change type of pagination in todo page to scrolling pagination

--- src/pages/TodoPage.vue
+++ src/pages/TodoPage.vue
@@ -2,13 +2,8 @@
   <h2 class="mb-4">
     Vue 3 App Todo Page
   </h2>
-  <div class="d-flex justify-content-center">
-    <AppPagination :page="page" :totalPage="totalPage" @changePage="changePage"/>
-  </div>
   <TodoList :todos="todos" />
-  <div class="d-flex justify-content-center">
-    <AppPagination :page="page" :totalPage="totalPage" @changePage="changePage"/>
-  </div>
+  <div ref="observer" class="observer"></div>
 </template>
 
 <script>
@@ -36,18 +31,32 @@ export default {
           }
         })
         this.totalPage = Math.ceil(response.headers['x-total-count'] / this.limit)
-        this.todos = [...response.data]
+        this.todos = [ ...this.todos, ...response.data]
       } catch (e) {
         alert('Error!')
       }
-    },
-    changePage(page) {
-      this.page = page
-      this.fetchTodoList()
     }
   },
   mounted() {
     this.fetchTodoList()
+
+    const options = {
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+    const callback = (entries, observer) => {
+      console.log('test')
+      if (entries[0].isIntersecting && this.page < this.totalPage) {
+        this.page++
+      }
+    };
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.observer)
+  },
+  watch: {
+    page() {
+      this.fetchTodoList()
+    }
   }
 }
 </script>
