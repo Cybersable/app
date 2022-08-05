8c6f16f4a45bafc7b00b129039e0612b12d9865c
Evgeniy <xmaoural@gmail.com>
2022-08-05 09:58:00 +0500

    Add pagination to Todo page

---
+++ src/components/UI/AppPagination.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item">
+        <a class="page-link" aria-label="Previous" @click="page--">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li
+        v-for="num in totalPage"
+        :key="num"
+        class="page-item"
+      >
+        <a
+          class="page-link"
+          @click="page=num"
+          :class="{active: page === num}"
+        >
+          {{ num }}
+        </a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" aria-label="Next" @click="page++">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "AppPagination",
+  props: {
+    page: {
+      required: true
+    },
+    totalPage: {
+      required: true
+    }
+  },
+  emits: ['changePage'],
+  watch: {
+    page(newVal) {
+      if (newVal > 0 && newVal <= this.totalPage)
+        this.$emit('changePage', newVal)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/TodoPage.vue
+++ src/pages/TodoPage.vue
@@ -1,29 +1,49 @@
 <template>
-  <h2>
+  <h2 class="mb-4">
     Vue 3 App Todo Page
   </h2>
+  <div class="d-flex justify-content-center">
+    <AppPagination :page="page" :totalPage="totalPage" @changePage="changePage"/>
+  </div>
   <TodoList :todos="todos" />
+  <div class="d-flex justify-content-center">
+    <AppPagination :page="page" :totalPage="totalPage" @changePage="changePage"/>
+  </div>
 </template>
 
 <script>
 import TodoList from "@/components/todo/TodoList";
 import axios from 'axios'
+import AppPagination from "@/components/UI/AppPagination";
 export default {
   name: "TodoPage",
-  components: {TodoList},
+  components: {AppPagination, TodoList},
   data() {
     return {
-      todos: []
+      todos: [],
+      page: 1,
+      limit: 15,
+      totalPage: 0
     }
   },
   methods: {
     async fetchTodoList() {
       try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        })
+        this.totalPage = Math.ceil(response.headers['x-total-count'] / this.limit)
         this.todos = [...response.data]
       } catch (e) {
         alert('Error!')
       }
+    },
+    changePage(page) {
+      this.page = page
+      this.fetchTodoList()
     }
   },
   mounted() {
